b3f3ce21a6f8f68af99a70c215e540a7ad29ac45
nurislamkalmanbetov <kalmanbetovnurislam19@gmail.com>
2024-10-08 20:07:46 +0600

    add APIs

--- core/settings.py
+++ core/settings.py
@@ -116,11 +116,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tour_api',
+        'USER': 'postgres',
+        'PASSWORD': 'qwerty123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
